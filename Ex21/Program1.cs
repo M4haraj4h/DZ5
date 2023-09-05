232632a051a47a18bf1ea451a345ddda103ef902
Шараев С.А <ssa@iro.ru>
2023-09-05 13:13:56 +0700

    1.5

---
+++ Ex21/Program1.cs
@@ -0,0 +1,15 @@
+﻿
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+int a = x2 - x1;
+int b = y2 - y1;
+int c = z2 - z1;
+
+double d = Math.Sqrt((a*a)+(b*b)+(c*c));
+
+Console.WriteLine(d);
